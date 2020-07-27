3863e3071a8ebc6b18ca9842ef59288f78ed3029
cbarbat <calin.barbat@web.de>
2020-07-27 20:18:24 +0200

    Commit 348 - Update bignum.c

--- src/bignum.c
+++ src/bignum.c
@@ -392,63 +392,39 @@ mscan(char *s)
 	return a;
 }
 
-// convert bignum to string
+// convert bignum to string (returned value points to static buffer)
 
 char *
 mstr(uint32_t *a)
 {
-	int k, n, r;
-	char c;
-
-	static char *str;
+	int i, k, n, r;
+	static char *buf;
 	static int len;
-
-	if (str == NULL) {
-		str = (char *) malloc(1000);
-		if (str == NULL)
-			malloc_kaput();
-		len = 1000;
-	}
-
-	// estimate string size
-
-	n = 10 * MLENGTH(a) + 1;
-
+	n = 10 * MLENGTH(a) + 1; // estimate string length
 	if (n > len) {
-		free(str);
-		str = (char *) malloc(n);
-		if (str == NULL)
+		if (buf)
+			free(buf);
+		buf = (char *) malloc(n);
+		if (buf == NULL)
 			malloc_kaput();
 		len = n;
 	}
-
-	a = mcopy(a);
-
 	k = len - 1;
-
-	str[k] = 0;
-
+	buf[k] = 0;
+	a = mcopy(a);
 	for (;;) {
-		k -= 9;
 		r = mdivby1billion(a);
-		c = str[k + 9];
-		sprintf(str + k, "%09d", r);
-		str[k + 9] = c; // restore char clobbered by sprintf
+		for (i = 0; i < 9; i++) {
+			buf[--k] = r % 10 + '0';
+			r /= 10;
+		}
 		if (MZERO(a))
 			break;
 	}
-
-	// remove leading zeroes
-
-	while (str[k] == '0')
-		k++;
-
-	if (str[k] == 0)
-		k--; // leave one leading zero
-
 	mfree(a);
-
-	return str + k;
+	while (k < len - 2 && buf[k] == '0') // remove leading zeroes
+		k++;
+	return buf + k;
 }
 
 // returns remainder as function value, quotient returned in a
@@ -457,14 +433,14 @@ int
 mdivby1billion(uint32_t *a)
 {
 	int i;
-	uint64_t kk = 0;
+	uint64_t u = 0;
 	for (i = MLENGTH(a) - 1; i >= 0; i--) {
-		kk = kk << 32 | a[i];
-		a[i] = (uint32_t) (kk / 1000000000); // compiler warns w/o cast
-		kk -= (uint64_t) 1000000000 * a[i];
+		u = u << 32 | a[i];
+		a[i] = (uint32_t) (u / 1000000000); // compiler warns w/o cast
+		u -= (uint64_t) 1000000000 * a[i];
 	}
 	mnorm(a);
-	return (int) kk; // compiler warns w/o cast
+	return (int) u; // compiler warns w/o cast
 }
 
 // returns u + v
